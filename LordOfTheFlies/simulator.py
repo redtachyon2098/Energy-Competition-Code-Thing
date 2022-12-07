4cf46fc4e543fe43bc5a41d3a2b414eb81ce7a0d
redtachyon2098 <redtachyon2718@gmail.com>
2022-12-07 23:28:06 +0900

    Update simulator.py
    Added logging, added comments.

--- LordOfTheFlies/simulator.py
+++ LordOfTheFlies/simulator.py
@@ -1,38 +1,40 @@
 import numpy as n
 import random as r
-import gc
-#_______________Every value and function worth modifying__________
+#_______________Stuff you'll modify a lot.__________
 verbose = True
-render = True
-stuff = True
-howmanytimesteps = 1000
-viewradius = 5
-biomesize = 30
-mapx = 200
-mapy = 200
-NPCs = [
+render = True #This requires pygame to enable, it will render the map and the players using one piexel per grid/player.
+logstuff = True #This makes the code export some game data to a file in the Log folder. It's not complete yet, and it will hog your RAM if you enable it.
+players = [ #The names of the actual players must be listed here in order for them to appear in the game. Add your folder to the Strategies folder and put their names here.
+    "ExampleStrats.Monkey",
+    "ExampleStrats.Collector"]
+howmanytimesteps = 1000 #How long do you want the game to run before it auto-terminates?
+viewradius = 5 #How far should the players be able to see?
+biomesize = 30 #How big should the biomes in the terrain generation be?
+mapx = 200 #How wide should the map be?
+mapy = 200 #How tall should the map be?
+NPCs = [ #All the foliage/other stuff that exist in the game. The raw code of each can be found in the Strategies/Nature folder. You can comment out specific objects to stop them from spawning. NOTE: Commenting out "Nature.Apple" without commenting out "Nature.Tree" or "Nature.Sequoia" will break a few things.
     "Nature.Tree",
     "Nature.Sequoia",
     "Nature.Apple",
     "Nature.Bush",
     "Nature.Fern",
     "Nature.Cactus"]
-NPCAmounts = [200, 200, 0, 100, 100, 100]
-NPCEnergy = [10, 10, 100, 100, 100, 100]
-NPCpermissions = [True, True, False, True, True, True]
-NPCConsumable = [False, False, True, True, True, True]
-NPCHostility = [False, False, False, False, False, False]
-players = [
-    "ExampleStrats.Monkey",
-    "ExampleStrats.Collector"]
-playerAmount = 1000
-playerEnergy = 1000
-howmanybiomes = 5 #oasis, desert, grass, jungle, mountain
-drainvalues = n.array([0.4, 0.6, 0.5, 0.6, 0.6])
-radiusvalues = n.array([3, 2, 3, 3, 2])
-biomecolors = n.array([[5,220, 100],[160,140,20],[40,220,5],[10,200,30],[10,80,10]])
+NPCAmounts = [200, 200, 0, 100, 100, 100] #How much should each non-player object initially spawn?
+NPCEnergy = [10, 10, 100, 100, 100, 100] #How much energy should each non-player onject initially have?
+playerAmount = 1000 #How many instances of each strategy should initially spawn?
+playerEnergy = 1000 #How much energy should each player initially have?
 
-def SpawnCondition(tile, name):
+#_____________Stuff you won't modify much._________________
+
+NPCpermissions = [True, True, False, True, True, True] #Which objects have admin permissions?(Currently used for spawning other objects, but can be used for literally anything.)
+NPCConsumable = [False, False, True, True, True, True] #Which objects are consumable? (You can't eat trees, for example.)
+NPCHostility = [False, False, False, False, False, False] #Which objects can attack players?
+howmanybiomes = 5 #oasis, desert, grass, jungle, mountain
+drainvalues = n.array([0.4, 0.6, 0.5, 0.6, 0.6]) #How much does moving around drain your energy? (Depending on the biome ID)
+radiusvalues = n.array([3, 3, 4, 3, 2]) #How far can you move every time step? (Depending on the biome ID)
+biomecolors = n.array([[5,220, 100],[160,140,20],[40,220,5],[10,200,30],[10,80,10]]) #What color should each biome be?
+
+def SpawnCondition(tile, name): #Where can NPOs spawn?
     if name == "Nature.Tree":
         return tile != 1
     elif name == "Nature.Sequoia":
@@ -46,10 +48,10 @@ def SpawnCondition(tile, name):
     else:
         return True
 
-def DrainEnergy(en, k, dist):
+def DrainEnergy(en, k, dist): #How much energy is drained when moving?
     return en - dist ** k - 0.1
 
-def combat(yourE, enemyE):
+def combat(yourE, enemyE): #How much energy do you gain when you attack something?
     if yourE > enemyE:
         return yourE + enemyE * 0.9
     else:
@@ -165,131 +167,161 @@ if render:
     p.init()
     display = p.display.set_mode((mapx, mapy), flags=p.SCALED)
     p.display.set_caption("islands lol")
+if logstuff:
+    finalbinary = bytes()
 running = True
-for timestep in range(howmanytimesteps):
-    if verbose:
-        clock = t.time()
-    mx = []
-    my = []
-    me = []
-    mm = []
-    mc = []
-    mcol = []
-    chasers = []
-    chasees = []
-    cx = []
-    cy = []
-    ce = []
-    cm = []
-    cc = []
-    ccol = []
-    for x in range(len(controller)): #General Movement
-        nx, ny, split, chasetarget, memory, splitmemory, ping = getinput(x, strs[controller[x]], xcoord[x], ycoord[x], energy[x], memories[x])
-        mx.append((xcoord[x] + nx) % mapx)
-        my.append((ycoord[x] + ny) % mapy)
-        if consu[controller[x]]:
-            me.append(DrainEnergy(energy[x], drain[xcoord[x], ycoord[x]], (nx ** 2 + ny ** 2)))
-        else:
+try:
+    for timestep in range(howmanytimesteps):
+        if verbose:
+            clock = t.time()
+        mx = []
+        my = []
+        me = []
+        mm = []
+        mc = []
+        mcol = []
+        chasers = []
+        chasees = []
+        cx = []
+        cy = []
+        ce = []
+        cm = []
+        cc = []
+        ccol = []
+        ci = []
+        for x in range(len(controller)): #General Movement
+            nx, ny, split, chasetarget, memory, splitmemory, ping = getinput(x, strs[controller[x]], xcoord[x], ycoord[x], energy[x], memories[x])
+            mx.append(nx)
+            my.append(ny)
             me.append(energy[x])
-        mm.append(memory)
-        mc.append(controller[x])
-        mcol.append(ping[0])
-        if chasetarget != -1:
-            chasers.append(x)
-            chasees.append(chasetarget)
-        if split: #Splitting
-            cx.append(mx[x])
-            cy.append(my[x])
-            me[x] /= 2
-            ce.append(me[x])
-            cm.append(splitmemory)
-            cc.append(mc[x])
-            ccol.append(mcol[x])
-        if perms[controller[x]] and len(ping) == 2: #Special permission to execute code
-            instruction = ping[-1]
-            exec(instruction, globals(), locals())
-    for x in range(len(chasers)): #Chasing
-        guy = chasers[x]
-        tar = chasees[x]
-        radius = radii[mx[guy], my[guy]]
-        dx = mx[tar] - mx[guy]
-        dy = my[tar] - my[guy]
-        if dx > 0:
-            if dx > mapx / 2:
-                dx = dx - mapx
-        if dx < 0:
-            if -dx > mapx / 2:
-                dx = dx + mapx
-        if dy > 0:
-            if dy > mapy / 2:
-                dy = dy - mapy
-        if dy < 0:
-            if -dy > mapy / 2:
-                dy = dy + mapy
-        dx = min(max(dx, -radius), radius)
-        dy = min(max(dy, -radius), radius)
-        mx[guy] = (mx[guy] + dx) % mapx
-        my[guy] = (my[guy] + dy) % mapy
-        me[guy] = DrainEnergy(me[guy], drain[mx[guy], my[guy]], (dx ** 2 + dy ** 2))
-    #Combine the OGs with the split clones
-    mx += cx
-    my += cy
-    me += ce
-    mm += cm
-    mc += cc
-    mcol += ccol
-    #Prune all dead players, turn list into numpy array.
-    alive = n.array(me) > 0
-    xco = n.array(mx)[alive]
-    yco = n.array(my)[alive]
-    eng = n.array(me)[alive]
-    con = n.array(mc)[alive]
-    mem = n.array(mm, dtype=object)[alive]
-    col = n.array(mcol)[alive]
-    hostile = hosti[con]
-    consumable = consu[con]
-    #Collision detection.
-    calcdE = []
-    for x in range(len(xco)):
-        onx = xco == xco[x]
-        ony = yco == yco[x]
-        foreign = con != con[x]
-        enemies = onx * ony * foreign * consumable
-        if enemies.any() and hostile[x]:
-            frontier = n.amax(eng[enemies])
-            newE = combat(eng[x], frontier)
-            calcdE.append(newE)
-        elif enemies.any() and consumable[x]:
-            frontier = n.amax(eng[enemies])
-            if frontier < eng[x]:
-                calcdE.append(eng[x])
+            mm.append(memory)
+            mc.append(controller[x])
+            mcol.append(ping[0])
+            if chasetarget != -1:
+                chasers.append(x)
+                chasees.append(chasetarget)
+            if split: #Splitting
+                cx.append(mx[x])
+                cy.append(my[x])
+                me[x] /= 2
+                ce.append(me[x])
+                cm.append(splitmemory)
+                cc.append(mc[x])
+                ccol.append(mcol[x])
+                ci.append(x)
+            if perms[controller[x]] and len(ping) == 2: #Special permission to execute code
+                instruction = ping[-1]
+                exec(instruction, globals(), locals())
+        for x in range(len(chasers)): #Chasing
+            guy = chasers[x]
+            tar = chasees[x]
+            radius = radii[mx[guy], my[guy]]
+            dx = mx[tar] - mx[guy]
+            dy = my[tar] - my[guy]
+            if dx > 0:
+                if dx > mapx / 2:
+                    dx = dx - mapx
+            if dx < 0:
+                if -dx > mapx / 2:
+                    dx = dx + mapx
+            if dy > 0:
+                if dy > mapy / 2:
+                    dy = dy - mapy
+            if dy < 0:
+                if -dy > mapy / 2:
+                    dy = dy + mapy
+            mx[guy] = dx
+            my[guy] = dy
+            #me[guy] = DrainEnergy(me[guy], drain[mx[guy], my[guy]], (dx ** 2 + dy ** 2))
+        #Combine the OGs with the split clones
+        oldinds = n.array(list(indices) + ci, dtype=int)
+        px = n.array(mx + cx)
+        py = n.array(my + cy)
+        me = n.array(me + ce)
+        mm = n.array(mm + cm, dtype=object)
+        mc = n.array(mc + cc)
+        mcol = n.array(mcol + ccol)
+        prex = xcoord[oldinds]
+        prey = ycoord[oldinds]
+        px = n.clip(px, -radii[prex, prey], radii[prex, prey])
+        py = n.clip(py, -radii[prex, prey], radii[prex, prey])
+        mx = (prex + px) % mapx
+        my = (prey + py) % mapy
+        filtering = consu[mc]
+        me[filtering] = DrainEnergy(me, drain[mx, my], py ** 2 + py ** 2)[filtering]
+        #Prune all dead players, turn list into numpy array.
+        alive = n.array(me) > 0
+        xco = mx[alive]
+        yco = my[alive]
+        eng = me[alive]
+        con = mc[alive]
+        mem = mm[alive]
+        col = mcol[alive]
+        hostile = hosti[con]
+        consumable = consu[con]
+        #Collision detection.
+        calcdE = []
+        for x in range(len(xco)):
+            onx = xco == xco[x]
+            ony = yco == yco[x]
+            foreign = con != con[x]
+            enemies = onx * ony * foreign * consumable
+            if enemies.any() and hostile[x]:
+                frontier = n.amax(eng[enemies])
+                newE = combat(eng[x], frontier)
+                calcdE.append(newE)
+            elif enemies.any() and consumable[x]:
+                frontier = n.amax(eng[enemies])
+                if frontier < eng[x]:
+                    calcdE.append(eng[x])
+                else:
+                    calcdE.append(0)
             else:
-                calcdE.append(0)
-        else:
-            calcdE.append(eng[x])
-    calcdE = n.array(calcdE)
-    nextgen = calcdE > 0
-    xcoord = xco[nextgen]
-    ycoord = yco[nextgen]
-    controller = con[nextgen]
-    energy = calcdE[nextgen]
-    memories = mem[nextgen]
-    colors = col[nextgen]
-    typ = typs[controller]
-    indices = n.arange(len(controller))
+                calcdE.append(eng[x])
+        calcdE = n.array(calcdE)
+        nextgen = calcdE > 0
+        xcoord = xco[nextgen]
+        ycoord = yco[nextgen]
+        controller = con[nextgen]
+        energy = calcdE[nextgen]
+        memories = mem[nextgen]
+        colors = col[nextgen]
+        typ = typs[controller]
+        indices = n.arange(len(controller))
+        if logstuff:
+            oldinds = oldinds[alive][nextgen]
+            indnums = list((oldinds  / 65536).astype(int)) +  list(((oldinds % 65536) / 256).astype(int)) +  list((oldinds % 256).astype(int))
+            hexn = indnums.copy()
+            if timestep == 0:
+                xcoordhex = list(((xcoord % 65536) / 256).astype(int)) + list(xcoord % 256)
+                ycoordhex = list(((ycoord % 65536) / 256).astype(int)) + list(ycoord % 256)
+                energyhex = list(((energy % 65536) / 256).astype(int)) + list(energy.astype(int) % 256)
+                hexn += xcoordhex + ycoordhex + energyhex
+            else:
+                px = px[alive][nextgen] + 8
+                py = py[alive][nextgen] + 8
+                hexn += list(px * 16 + py)
+            hexn = [int((len(hexn) % 65536) / 256), len(hexn) % 256] + hexn
+            finalbinary += bytes(hexn)
+        if render:
+            screen = colormap.copy()
+            screen[xcoord, ycoord] = colors
+            surf = p.surfarray.make_surface(screen)
+            for event in p.event.get():
+                if event.type == p.QUIT:
+                    running = False
+            display.blit(surf, (0, 0))
+            p.display.update()
+        if verbose:
+            print(1 / (t.time() - clock), "FPS at", len(controller) / (t.time() - clock), "PPS")
+        if logstuff:
+            print("")
     if render:
-        screen = colormap.copy()
-        screen[xcoord, ycoord] = colors
-        surf = p.surfarray.make_surface(screen)
-        for event in p.event.get():
-            if event.type == p.QUIT:
-                running = False
-        display.blit(surf, (0, 0))
-        p.display.update()
-        gc.collect()
-    if verbose:
-        print(1 / (t.time() - clock), "FPS at", len(controller) / (t.time() - clock), "PPS")
-    if logstuff:
-        print("")
-if render:
-    p.quit()
+        p.quit()
+except KeyboardInterrupt:
+    print("Terminated.")
+if logstuff:
+    file = open("Log/BinaryExport.bin","wb")
+    file.write(finalbinary)
+    file.close()
+    print("Exported log.")
