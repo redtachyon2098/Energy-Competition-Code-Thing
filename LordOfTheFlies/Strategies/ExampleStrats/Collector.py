c0d3eff804ba360cecf54351066137b68cc6af99
redtachyon2098 <redtachyon2718@gmail.com>
2022-12-11 20:10:53 +0900

    Fixes and Improvements
    Fixed a bug where it cripples the strategy, and slowed its splitting rate.

--- LordOfTheFlies/Strategies/ExampleStrats/Collector.py
+++ LordOfTheFlies/Strategies/ExampleStrats/Collector.py
@@ -5,17 +5,17 @@ def strategy(tile, nearx, neary, types, energies, yourenergy, memory):
     target = -1
     if len(energies) > 0:
         couldbenice = energies < 0.9 * yourenergy
-        couldbeeaten = (types != 0) * (types != 1) * (types != 2)
+        couldbeeaten = (types != 0) * (types != 1) * (types != -2)
         menu = energies * couldbenice * couldbeeaten
         if not (menu == 0).all():
             target = n.where(menu == n.amax(menu))[0][0]
     dx = 0
     dy = 0
-    if target != -1:
+    if target == -1:
         #print(target)
         dx = r.randint(-3,3)
         dy = r.randint(-3,3)
     if len(energies) > 0:
-        return n.array([dx, dy]), 0.9 * yourenergy > 2 * n.amax(energies), target, [], [], [[255,255,255]]
+        return n.array([dx, dy]), 0.01 * yourenergy > 2 * n.amax(energies), target, [], [], [[255,255,255]]
     else:
         return n.array([dx, dy]), False, target, [], [], [[255,255,255]]
