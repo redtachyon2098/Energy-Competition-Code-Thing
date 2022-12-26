e3014fcaed1555881a0386b8a10c2c0058f05c7e
redtachyon2098 <redtachyon2718@gmail.com>
2022-12-26 16:22:28 +0900

    Forgot to update!!

--- LordOfTheFlies/Strategies/Nature/Cactus.py
+++ LordOfTheFlies/Strategies/Nature/Cactus.py
@@ -3,6 +3,6 @@ import random as r
 
 def strategy(tile, nearx, neary, types, energies, yourenergy, memory):
     if r.random() < 0.02:
-        return n.array([0,0]), False, -1, [], [], [[0, 90, 10], "sx = (mx[x] + r.randint(-4,4)) % mapx\nsy = (my[x] + r.randint(-4,4)) % mapx\nif SpawnCondition(biome[sx, sy], 'Nature.Bush'):\n\tcx.append(sx)\n\tcy.append(sy)\n\tce.append(energ[5])\n\tcm.append([])\n\tcc.append(5)\n\tccol.append([0, 90, 10])"]
+        return n.array([0,0]), False, -1, [], [], [[0, 90, 10], "sx = (mx[x] + r.randint(-4,4)) % mapx\nsy = (my[x] + r.randint(-4,4)) % mapx\nif SpawnCondition(biome[sx, sy], 'Nature.Bush'):\n\tcx.append(sx)\n\tcy.append(sy)\n\tce.append(energ[5])\n\tcm.append([])\n\tcc.append(5)\n\tccol.append([0, 90, 10])\n\tci.append(x)"]
     else:
         return n.array([0,0]), False, -1, [], [], [[0, 90, 10], ""]
