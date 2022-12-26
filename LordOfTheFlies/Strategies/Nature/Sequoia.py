93630a205af26508fe66fdb9bfdda921ae42c6b8
redtachyon2098 <redtachyon2718@gmail.com>
2022-12-26 16:23:31 +0900

    Forgot to update!!

--- LordOfTheFlies/Strategies/Nature/Sequoia.py
+++ LordOfTheFlies/Strategies/Nature/Sequoia.py
@@ -3,6 +3,6 @@ import numpy as n
 
 def strategy(tile, nearx, neary, types, energies, yourenergy, memory):
     if r.random() < 0.02:
-        return n.array([0,0]), False, -1, [], [], [[100, 110, 20], "cx.append((mx[x] + r.randint(-2,2)) % mapx)\ncy.append((my[x] + r.randint(-2,2)) % mapy)\nce.append(energ[2])\ncm.append([])\ncc.append(2)\nccol.append([200,10,10])"]
+        return n.array([0,0]), False, -1, [], [], [[100, 110, 20], "cx.append((mx[x] + r.randint(-2,2)) % mapx)\ncy.append((my[x] + r.randint(-2,2)) % mapy)\nce.append(energ[2])\ncm.append([])\ncc.append(2)\nccol.append([200,10,10])\nci.append(x)"]
     else:
         return n.array([0,0]), False, -1, [], [], [[100, 110, 20], ""]
