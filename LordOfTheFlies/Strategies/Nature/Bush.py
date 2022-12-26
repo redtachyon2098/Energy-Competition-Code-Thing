d0051da0719bec3ac4dfeb53a62760a18a2dfe4d
redtachyon2098 <redtachyon2718@gmail.com>
2022-12-26 16:21:54 +0900

    Forgot to update!!

--- LordOfTheFlies/Strategies/Nature/Bush.py
+++ LordOfTheFlies/Strategies/Nature/Bush.py
@@ -3,6 +3,6 @@ import random as r
 
 def strategy(tile, nearx, neary, types, energies, yourenergy, memory):
     if r.random() < 0.02:
-        return n.array([0,0]), False, -1, [], [], [[40, 100, 100], "sx = (mx[x] + r.randint(-4,4)) % mapx\nsy = (my[x] + r.randint(-4,4)) % mapx\nif SpawnCondition(biome[sx, sy], 'Nature.Bush'):\n\tcx.append(sx)\n\tcy.append(sy)\n\tce.append(energ[3])\n\tcm.append([])\n\tcc.append(3)\n\tccol.append([40, 100, 100])"]
+        return n.array([0,0]), False, -1, [], [], [[40, 100, 100], "sx = (mx[x] + r.randint(-4,4)) % mapx\nsy = (my[x] + r.randint(-4,4)) % mapx\nif SpawnCondition(biome[sx, sy], 'Nature.Bush'):\n\tcx.append(sx)\n\tcy.append(sy)\n\tce.append(energ[3])\n\tcm.append([])\n\tcc.append(3)\n\tccol.append([40, 100, 100])\n\tci.append(x)"]
     else:
         return n.array([0,0]), False, -1, [], [], [[40, 100, 100], ""]
